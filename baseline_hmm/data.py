5149527e351c6778efc843922abaef5db9834390
ipinmi <ipinmiadebayo@gmail.com>
2024-06-01 08:09:40 +0200

    added random tagger and reported tag distribution

--- baseline_hmm/data.py
+++ baseline_hmm/data.py
@@ -1,15 +1,10 @@
+from collections import defaultdict
+
+
 def read_data_file(file_name):
     """
     Reading the CONNLU format file in the following format:
     [(word1, tag1), (word2, tag2), ...] for each sentence.
-
-    Parameters:
-        file_name: str
-            The name of the file to read
-
-    Returns:
-        data: list[list[tuple]]
-            A list of lists of tuples for each word and its corresponding tag in each sentence
     """
     with open(file_name, "r") as file:
         lines = file.readlines()
@@ -31,17 +26,6 @@ def extract_tokens_tags(data):
     """
     Extracting the tags from the data for evaluation in the following format:
     [tag1, tag2, ...] for each sentence.
-
-    Parameters:
-        data: list[list[tuple]]
-
-    Returns:
-        token_sequence: list[list]
-            A list of lists of tokens for each sentence
-        sentence_tags: list[list]
-            A list of lists of tags for each sentence
-        all_tags_in_data: list
-            A merged list of all tags in the data
     """
     sentence_tags = [[tag for _, tag in sentence] for sentence in data]
     token_sequence = [[word for word, _ in sentence] for sentence in data]
@@ -50,3 +34,18 @@ def extract_tokens_tags(data):
     all_tags_in_data = [tag for sentence in sentence_tags for tag in sentence]
 
     return token_sequence, sentence_tags, all_tags_in_data
+
+
+# Distribution of the tags
+def tag_distribution(tag_sequences):
+    """
+    Returns the distribution of the tags in the data in the following format:
+    {tag1: count1, tag2: count2, ...}
+    """
+    tag_distribution = defaultdict(int)
+    for tag in tag_sequences:
+        if tag in tag_distribution:
+            tag_distribution[tag] += 1
+        else:
+            tag_distribution[tag] = 1
+    return dict(sorted(tag_distribution.items()))
---
+++ baseline_hmm/dummy_tagger.py
@@ -0,0 +1,38 @@
+import numpy as np
+from data import read_data_file, extract_tokens_tags, tag_distribution
+from evaluation import Evaluation
+
+# Read the appropriate experiment file to get the gold standard tags
+filename = "dataset/test.col"
+gold_standard_data = read_data_file(filename)
+sentences, _, gold_standard_tags = extract_tokens_tags(gold_standard_data)
+
+
+# assign random tags to the tokens in each sentence
+sentence_predicted_tags = []
+for sentence in sentences:
+    sentence_predicted_tags.append(np.random.choice(gold_standard_tags, len(sentence)))
+
+all_predicted_tags = [tag for tags in sentence_predicted_tags for tag in tags]
+
+prediction_distribution = tag_distribution(all_predicted_tags)
+gold_standard_distribution = tag_distribution(gold_standard_tags)
+
+# Evaluate the predicted tags against the gold standard tags
+evaluator = Evaluation(all_predicted_tags, gold_standard_tags)
+# Calculate precision, recall, and F1 score for all predicted tags
+precision, recall, f1_score = evaluator.precision_recall_fScore()
+
+
+# Write the evaluation results to a file
+with open("random_tagging_evaluation_results.txt", "a") as file:
+    if "test" in filename:
+        file.write("Evaluation results for the test data:\n")
+    else:
+        file.write("Evaluation results for the dev data:\n")
+    file.write(f"Gold Standard Tags Distribution: {gold_standard_distribution}\n")
+    file.write(f"Predicted Tags Distribution: {prediction_distribution}\n")
+    file.write(f"Precision {np.round(precision, 2)}\n")
+    file.write(f"Recall: {np.round(recall,2)}\n")
+    file.write(f"F1 Score: {np.round(f1_score,2)}\n")
+    file.write("\n")
